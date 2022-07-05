02efb4d63316f653cbd1fa697f4e0c07c7336d75
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-07-05 20:56:13 -0300

    Create menu structure

--- src/assets/components/primary-menu/index.jsx
+++ src/assets/components/primary-menu/index.jsx
@@ -1,7 +1,61 @@
+import styled from "styled-components";
+import { MdExpandMore } from "react-icons/md";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export const PrimaryMenu = () => {
-    return (
-        <ul>
-            <li>menu</li>
-        </ul>
-    )
-}
+  const [servicesIsClicked, setServicesIsClicked] = useState(false);
+
+  console.log(servicesIsClicked);
+
+  return (
+    <Menu>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>Sobre</li>
+      <ServicesItem onClick={() => setServicesIsClicked(!servicesIsClicked)}>
+        <NameAndExpandIcon>
+          <Link to="/">Serviços</Link>
+          <MdExpandMore />
+        </NameAndExpandIcon>
+
+        <SubMenu isClicked={servicesIsClicked}>
+          <li>
+            <Link to="/aulas-de-musica">Aulas de Música</Link>
+          </li>
+          <li>
+            <Link to="/musicoterapia">Musicoterapia</Link>
+          </li>
+          <li>
+            <Link to="/palestras">Palestras</Link>
+          </li>
+        </SubMenu>
+      </ServicesItem>
+      <li>
+        <Link to="/contato">Contato</Link>
+      </li>
+    </Menu>
+  );
+};
+
+export const Menu = styled.ul`
+  display: flex;
+  position: relative;
+  gap: 30px;
+  font-weight: 400;
+`;
+
+export const ServicesItem = styled.li``;
+
+export const NameAndExpandIcon = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+export const SubMenu = styled.ul`
+  color: red;
+  position: absolute;
+  ${({ isClicked }) => (isClicked ? { display: "block" } : { display: "none" })}
+`;
